e956b2d3ec6e1c01b5c3d05afc9f516812b1a505
Your Name <mr.shikhar100@gmail.com>
2026-06-09 00:16:52 +0530

    Doctor routes changes

--- HealthCare-backend/routes/doctor.routes.js
+++ HealthCare-backend/routes/doctor.routes.js
@@ -1,11 +1,11 @@
 const express=require("express")
 const router=express.Router()
 const {registerDoctorProfile,getAllDoctors,getDoctorById}=require("../controllers/doctor.controllers")
-const role = require("../middlewares/auth.middlewares")
-const auth = require("../middlewares/role.middlewares")
+const auth = require("../middlewares/auth.middlewares")
+const role = require("../middlewares/role.middlewares")
 
 
-router.post("/profile", role, auth("doctor"), registerDoctorProfile)
+router.post("/profile",auth, role("doctor"), registerDoctorProfile)
 router.get("/",getAllDoctors)
 router.get("/:id",getDoctorById)
 
